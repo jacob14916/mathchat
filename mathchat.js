f92ba2afbc91125098b7acc9a73ca243354af1b2
Jacob Zhang <jacob.b.zhang@gmail.com>
2015-07-06 18:51:10 -0700

    Implemented basic chat (no math)

--- mathchat.js
+++ mathchat.js
@@ -1,17 +1,40 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.chatarea.helpers({
+    messages: function() {
+      return Messages.find({}, {sort: ["createdAt"]});
+    }
+  });
+  
+  Template.chatarea.events({
+    'submit .newchat': function (evt, inst) {
+      var chatdiv = inst.find(".chatarea");
+      chatdiv.scrollTop = chatdiv.scrollHeight;
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.chat.helpers({
+    user: "user",
+    time: function() {
+      var date = this.createdAt;
+      return date.getMonth() + "/" + date.getDay() + "@" +
+	date.getHours() + ":" + date.getMinutes();
+    }
+  });
+
+  Template.textentry.events({
+    'submit .newchat': function (evt) {
+      var text = evt.target.text.value;
+      Messages.insert({
+	text: text,
+	createdAt: new Date()
+      });
+      evt.target.text.value = "";
+      evt.preventDefault();
     }
   });
 }
@@ -19,5 +42,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    
   });
 }
